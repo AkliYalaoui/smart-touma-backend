a5a7610dd71cab464ed3910d8c671ad884d4a53a
AkliYalaoui <akliyalaoui@gmail.com>
2024-07-29 16:48:11 +0200

    Added auth and upload middlwares

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -1,26 +1,32 @@
 
 const admin = require('firebase-admin');
+const {StatusCodes} = require("http-status-codes");
 const {config} = require('dotenv');
 config();
 
 const verifyTokenAndApiKey = async (req, res, next) => {
     const token = req.headers['authorization'];
     const apiKey = req.headers['x-api-key'];
-  
-    if (!token) {
-      return res.status(403).send('A token is required for authentication');
+
+    const devENV = process.env.ENV ==  "dev";
+    if (!devENV && !token) {
+      return res.status(StatusCodes.FORBIDDEN).json({error : 'A token is required for authentication'});
     }
   
     if (!apiKey || apiKey !== process.env.API_KEY) {
-      return res.status(403).send('Invalid API Key');
+      return res.status(StatusCodes.FORBIDDEN).send('Invalid API Key');
     }
   
     try {
-      const decodedToken = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
-      req.user = decodedToken;
+      if(!devENV){
+        const decodedToken = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+        req.uid = decodedToken.uid;
+      }else{
+        req.uid = "QKzka1CihLO393nu765YXJmL00s2";
+      }
       return next();
     } catch (error) {
-      return res.status(401).send('Invalid Token');
+      return res.status(StatusCodes.UNAUTHORIZED).json({error : 'Invalid Token'});
     }
   };
   
