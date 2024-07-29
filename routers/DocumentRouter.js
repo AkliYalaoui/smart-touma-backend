5668f3c8db9481e448c4e403af719b3886073747
AkliYalaoui <akliyalaoui@gmail.com>
2024-07-29 16:48:40 +0200

    Protect documents routes

--- routers/DocumentRouter.js
+++ routers/DocumentRouter.js
@@ -1,10 +1,9 @@
 const router = require("express").Router();
 const verifyTokenAndApiKey = require('../middlewares/AuthMiddleware.js');
+const upload = require('../middlewares/ImageMiddleware.js');
 const {getDocuments, processImages} = require("../controllers/DocumentController.js");
 
-router.get("/", getDocuments);
-router.post("/process", verifyTokenAndApiKey, processImages);
-
-
+router.get("/", verifyTokenAndApiKey, getDocuments);
+router.post("/process", verifyTokenAndApiKey, upload, processImages);
 
 module.exports = router;
