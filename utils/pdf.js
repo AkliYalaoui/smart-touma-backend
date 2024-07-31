52c0503323c8b21ac4e28f023258c082ded94f59
AkliYalaoui <akliyalaoui@gmail.com>
2024-08-01 00:52:31 +0200

    removing non-empty directories

--- utils/pdf.js
+++ utils/pdf.js
@@ -2,27 +2,24 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 const tmp = require("tmp");
+const rimraf = require("rimraf");
 
+const generatePDF = (latexCode) => {
+  return new Promise((resolve, reject) => {
+    const tmpDir = tmp.dirSync({ unsafeCleanup: true });
+    const texFilePath = path.join(tmpDir.name, "document.tex");
+    const pdfFilePath = path.join(tmpDir.name, "document.pdf");
 
-const generate = (latexCode) => {
-    return new Promise((resolve, reject) => {
-      const tmpDir = tmp.dirSync({ unsafeCleanup: true });
-      const texFilePath = path.join(tmpDir.name, "document.tex");
-      const pdfFilePath = path.join(tmpDir.name, "document.pdf");
-  
-      fs.writeFileSync(texFilePath, latexCode);
-  
-      exec(`pdflatex -output-directory=${tmpDir.name} ${texFilePath}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error("Error generating PDF:", error);
-          console.error(stderr);
-          tmpDir.removeCallback();
-          reject(new Error("Error generating PDF"));
-        } else {
-          resolve({ pdfFilePath, cleanupCallback: tmpDir.removeCallback });
-        }
-      });
+    fs.writeFileSync(texFilePath, latexCode);
+
+    exec(`pdflatex -output-directory=${tmpDir.name} ${texFilePath}`, (error, stdout, stderr) => {
+      if (error) {
+        console.error("Error generating PDF:", error);
+        console.error(stderr);
+        rimraf(tmpDir.name, () => reject(new Error("Error generating PDF")));
+      } else {
+        resolve({ pdfFilePath, cleanupCallback: () => rimraf(tmpDir.name, () => {}) });
+      }
     });
-  };
-
-module.exports = {generate}
+  });
+};
